144d6c6e46122e1fde2931bea95f5c5d28fab745
alujan06 <alejandrolujanaguilar@MacBook-Air-de-Alejandro.local>
2026-06-05 11:01:15 +0200

    Feat: Desarrollado el Buscador Galáctico inteligente inyectando IDs dinámicos en la API con comillas inclinadas

--- apis-fetch practicas/js/app.js
+++ apis-fetch practicas/js/app.js
@@ -20,7 +20,7 @@
 // const texto = document.querySelector('#texto-consejo');
 
 // async function obtenerConsejo() {
-//     const respuesta = await fetch(' https://api.adviceslip.com/advice')
+//     const respuesta = await fetch('https://api.adviceslip.com/advice')
 //     const datos = await respuesta.json();
 //     console.log(datos);
 //     texto.textContent = datos.slip.advice;
@@ -28,4 +28,22 @@
 
 // boton.addEventListener('click', () => {
 //     obtenerConsejo();
+// });
+
+// Practica 3
+
+// const id = document.querySelector('#cajaId');
+// const boton = document.querySelector('#btnBuscar');
+// const titulo = document.querySelector('#pantallaNombre');
+
+// async function obtenerDatos() {
+//     let idBuscado = id.value;
+//     const respuesta = await fetch(`https://swapi.dev${idBuscado}/api/people/`);
+//     const datos = await respuesta.json();
+//     console.log(datos);
+//     titulo.textContent = datos.name;
+// }
+
+// boton.addEventListener('click', () => {
+//     obtenerDatos();
 // });
