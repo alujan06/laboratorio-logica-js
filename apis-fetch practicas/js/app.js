80291577760ae45abb3a530c263dfba689643326
alujan06 <alejandrolujanaguilar@MacBook-Air-de-Alejandro.local>
2026-06-05 10:43:07 +0200

    Hito Histórico: Conectadas mis primeras llamadas asíncronas con internet usando async/await y fetch hacia APIs externas

---
+++ apis-fetch practicas/js/app.js
@@ -0,0 +1,31 @@
+// Practica 1
+
+// const boton = document.querySelector('#btnTraer');
+// const titulo = document.querySelector('#pantalla');
+
+// async function obtenerDatos() {
+//     const respuesta = await fetch('https://typicode.com');
+//     const datos = await respuesta.json();
+//     console.log(datos);
+//     titulo.textContent = datos.name;
+// }
+
+// boton.addEventListener('click', () => {
+//     obtenerDatos();
+// });
+
+// Practica 2
+
+// const boton = document.querySelector('#btnConsejo');
+// const texto = document.querySelector('#texto-consejo');
+
+// async function obtenerConsejo() {
+//     const respuesta = await fetch(' https://api.adviceslip.com/advice')
+//     const datos = await respuesta.json();
+//     console.log(datos);
+//     texto.textContent = datos.slip.advice;
+// }
+
+// boton.addEventListener('click', () => {
+//     obtenerConsejo();
+// });
